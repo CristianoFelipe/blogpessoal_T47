61c75156e95d7a7b73bd3b074904d8d28a9534c5
CristianoFelipe <cristianofnunes97@gmail.com>
2022-04-02 11:42:20 -0300

    Crud

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,8 @@
+
 package com.generation.blogpessoal.repository;
 
 import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,8 +13,6 @@ import com.generation.blogpessoal.model.Postagem;
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
 	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
-<<<<<<< HEAD:src/main/java/com/generation/blogpessoal/repository/PostagemRepository_.java
+
+	
 }
-=======
-}
->>>>>>> 46b6cd92758bf6eff21cf827fae2f199d2b60484:src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
